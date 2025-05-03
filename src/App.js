1abcfa5bef2b76a34f1a1628af6eaf9541e65b97
Farai Matangira <>
2025-05-02 22:32:42 -0500

    Made some changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Hero from './components/hero/hero.component';
 import OurStory from './routes/our-story/our-story.component';
 import LatestMusic from './routes/latest-music/latest-music.component';
 import { Route, Routes } from 'react-router-dom';
-import { Outlet } from 'react-router-dom';
+import SpotifyEmbed from './components/spotify-embed/spotify-embed.component';
 
 function App() {
 
@@ -15,6 +15,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Hero />} />
+        <Route index element={<SpotifyEmbed />} />
         <Route path='/our-story' element={<OurStory />}/>
         <Route path='/latest-music' element={<LatestMusic />}/>
       </Route>
